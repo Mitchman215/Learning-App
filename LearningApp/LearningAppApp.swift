93ef4c444018fab812e3205b3781ec10d68bc505
Mitchell Salomon <mitchell_salomon@brown.edu>
2021-07-07 23:00:00 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Mitchell Salomon on 7/7/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
