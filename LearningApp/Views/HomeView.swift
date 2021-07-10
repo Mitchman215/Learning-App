8d60909265370974a0c41c13e62c017bf7a40b42
Mitchell Salomon <mitchell_salomon@brown.edu>
2021-07-09 21:14:43 -0400

    - Create HomeView Card View to house each card in the home view - Layout basic HomeView appearance

--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -12,13 +12,45 @@ struct HomeView: View {
     @EnvironmentObject var model:ContentModel
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            VStack (alignment: .leading) {
+                
+                Text("What do you want to do today?")
+                    .padding(.leading, 20)
+                
+                ScrollView {
+                    LazyVStack {
+                        ForEach (model.modules) { module in
+                            VStack (spacing: 20) {
+                                // MARK: Learning Card
+                                HomeViewCard(image: module.content.image,
+                                             title: "Learn \(module.category)",
+                                             description: module.content.description,
+                                             count: "\(module.content.lessons.count) Lessons",
+                                             time: module.content.time)
+                                
+                                // MARK: Test Card
+                                HomeViewCard(image: module.test.image,
+                                             title: "\(module.category) Test",
+                                             description: module.test.description,
+                                             count: "\(module.test.questions.count) Lessons",
+                                             time: module.test.time)
+                            }
+                        }
+                        
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Get Started")
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
     }
 }
---
+++ LearningApp/Views/HomeViewCard.swift
@@ -0,0 +1,82 @@
+//
+//  HomeViewCard.swift
+//  LearningApp
+//
+//  Created by Mitchell Salomon on 7/9/21.
+//
+
+import SwiftUI
+
+struct HomeViewCard: View {
+    
+    var image: String
+    var title: String
+    var description: String
+    var count: String
+    var time: String
+    
+    var body: some View {
+        
+        ZStack {
+            
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+            
+            HStack {
+                // Image
+                Image(image)
+                    .resizable()
+                    .frame(width: 116, height: 116)
+                    .clipShape(Circle())
+                
+                Spacer()
+                
+                // Text
+                VStack(alignment: .leading, spacing: 10) {
+                    // Headline
+                    Text(title)
+                        .bold()
+                    
+                    // Description
+                    Text(description)
+                        .padding(.bottom, 20)
+                        .font(.caption)
+                    
+                    // Icons
+                    HStack {
+                        // Number of lessons/questions
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(count)
+                            .font(Font.system(size: 10))
+                        
+                        Spacer()
+                        
+                        // Time
+                        Image(systemName: "clock")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(time)
+                            .font(Font.system(size: 10))
+                        
+                    }
+                    
+                }
+                .padding(.leading, 20)
+            }
+            .padding(.horizontal, 20)
+            
+        }
+        
+    }
+}
+
+struct HomeViewCard_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewCard(image: "swift", title: "Learn Swift", description: "some description", count: "10 lessons", time: "2 hours")
+    }
+}
