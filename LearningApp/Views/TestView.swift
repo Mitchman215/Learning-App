9e2610f0f07a182723b3393c6be96431e47a01ff
Mitchell Salomon <mitchell_salomon@brown.edu>
2021-07-11 22:52:44 -0400

    - create interface for selecting question answers in the test view - create submit button to submit selected answer - implement logic to determine correct answer, update ui accordingly, and track how many answers the user gets right

--- LearningApp/Views/TestView.swift
+++ LearningApp/Views/TestView.swift
@@ -10,22 +10,86 @@ import SwiftUI
 struct TestView: View {
     
     @EnvironmentObject var model: ContentModel
+    @State var selectedAnswerIndex: Int?
+    @State var numCorrect = 0
+    @State var submitted = false
     
     var body: some View {
         
         if model.currentQuestion != nil {
-            VStack {
+            VStack (alignment: .leading){
                 
                 // Question number
                 Text("Question \(model.currentQuestionIndex + 1) of \(model.currentModule?.test.questions.count ?? 0)")
+                    .padding(.leading, 20)
                 
-                // Question
+                // Question text
                 CodeTextView()
+                    .padding(.horizontal, 20)
                 
                 // Answers
-                
+                ScrollView {
+                    VStack {
+                        ForEach (0..<model.currentQuestion!.answers.count) { index in
+                            
+                            Button {
+                                // Track the selected answer index
+                                selectedAnswerIndex = index
+                            } label: {
+                                ZStack {
+                                    if !submitted {
+                                        RectangleCard(color: index == selectedAnswerIndex ? .gray : .white)
+                                            .frame(height: 48)
+                                    }
+                                    else { // Answer has been submitted
+                                        // show green background for correct answer
+                                        if index == model.currentQuestion!.correctIndex {
+                                            RectangleCard(color: .green)
+                                                .frame(height: 48)
+                                        }
+                                        // show red background if user selected answer isn't correct
+                                        else if index == selectedAnswerIndex {
+                                            RectangleCard(color: .red)
+                                                .frame(height: 48)
+                                        }
+                                        // shows white background on all other answers
+                                        else {
+                                            RectangleCard()
+                                                .frame(height: 48)
+                                        }
+                                    }
+                                    
+                                    Text(model.currentQuestion!.answers[index])
+                                }
+                            }
+                            .disabled(submitted)
+                        }
+                    }
+                    .accentColor(.black)
+                    .padding()
+                }
                 
                 // button to advance
+                Button {
+                    // Change submitted state to true
+                    submitted = true
+                    
+                    // Check answer
+                    if selectedAnswerIndex == model.currentQuestion!.correctIndex {
+                        numCorrect += 1
+                    }
+                } label: {
+                    ZStack {
+                        RectangleCard(color: .green)
+                            .frame(height: 48)
+                        Text("Submit")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                    .padding()
+                }
+                .disabled(selectedAnswerIndex == nil)
+
             }
             .navigationTitle("\(model.currentModule?.category ?? "") Test")
         }
